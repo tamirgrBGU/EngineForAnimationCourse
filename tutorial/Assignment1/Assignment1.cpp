d14b774434bb7cbb9795fb6437a5137c9537027e
nadavhaik <nadavhb@gmail.com>
2022-12-01 10:40:49 +0200

    Update Assignment1.cpp
    display name

--- tutorial/Assignment1/Assignment1.cpp
+++ tutorial/Assignment1/Assignment1.cpp
@@ -19,7 +19,7 @@ int main()
     const float FAR = 120.0f;
 
     Renderer renderer;
-    Display display("Example1", DISPLAY_WIDTH, DISPLAY_HEIGHT, &renderer);
+    Display display("Assignment1 - Mesh Simplificator", DISPLAY_WIDTH, DISPLAY_HEIGHT, &renderer);
     std::cout.setstate(std::ios_base::failbit); // suppress junk output to console from igl::opengl::glfw::Viewer
     auto scene = std::make_shared<SCENE>(STRINGIFY(SCENE), &display);
     std::cout.clear(); // re-enable output to console
